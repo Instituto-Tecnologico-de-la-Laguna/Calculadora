175e4bc635da0913a808efee373871fbf233f326
PepeBeto309 <carrillo.jose.dev@gmail.com>
2025-08-28 15:31:36 -0600

    Se agrego Boton de Igual

--- Calculadora/Form1.Designer.cs
+++ Calculadora/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(550, 332);
+            this.button1.Location = new System.Drawing.Point(553, 311);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(215, 97);
             this.button1.TabIndex = 0;
