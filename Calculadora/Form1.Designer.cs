e2dd822abb79a3197786fd13ea40cc2c7ace715d
isaacfavel <pirrisfavela@gmail.com>
2025-08-28 15:41:28 -0600

    se agrega pantalla form2

--- Calculadora/Form1.Designer.cs
+++ Calculadora/Form1.Designer.cs
@@ -56,7 +56,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1010, 641);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
